b82b1b60a2c1d09fafab0a2409597c8fea7f7ee6
HEHvidunge <hh222zd@student.lnu.se>
2023-10-01 13:25:23 +0200

    sunday1okt3

--- L4/js/script.js
+++ L4/js/script.js
@@ -33,42 +33,43 @@ class ImageViewer {
 	getImages(XMLcode) { // Parametern XMLcode är hela den inlästa XML-koden
 		let urlElems = XMLcode.getElementsByTagName("url"); // Alla url-element
 		let captionElems = XMLcode.getElementsByTagName("caption"); // Alla caption-element
-		//let titleElem = XMLcode.getElementsByTagName("imagelist");
-		console.log(XMLcode.getElementsByTagName("category")[0].firstChild.data);
+	
+		console.log("category",XMLcode.getElementsByTagName("category")[0].firstChild.data);
 
-		imgViewer.titleElem = XMLcode.getElementsByTagName("category")[0].firstChild.data;
-		console.log(imgViewer.titleElem);
-		imgViewer.list.imgUrl = [];
-		imgViewer.list.imgCaption = [];
+		imgViewer.category = XMLcode.getElementsByTagName("category")[0].firstChild.data;//Hämtar kategorinamnet
+		this.titleElem.innerHTML = imgViewer.category;//Visar kategorinamnet
+		console.log("imgViewer",imgViewer.category);
+		imgViewer.list.imgUrl = [];//Tömmer bildlistan
+		imgViewer.list.imgCaption = [];//Tömmer bildtextlistan
 		for (let i = 0; i < urlElems.length; i++) {
 			imgViewer.list.imgUrl[i] = (urlElems[i].firstChild.data);
 			imgViewer.list.imgCaption[i] = (captionElems[i].firstChild.data);
 
 		}
 		this.imgIx = 0;
-		//console.log(imgViewer.titleElem);
-		this.showImage(); // Visa första bilden
+		
+		this.showImage(); // Anropar funktion för att visa första bilden
 	}; // Slut
 
 	// Visa bilden med index imgIx
 	showImage() {
 
-		this.imgElem.src = this.list.imgUrl[this.imgIx];
+		this.imgElem.src = this.list.imgUrl[this.imgIx];//Visar bilden
 
-		this.captionElem.innerHTML = (this.imgIx + 1) + ". " + this.list.imgCaption[this.imgIx];
+		this.captionElem.innerHTML = (this.imgIx + 1) + ". " + this.list.imgCaption[this.imgIx]; //Visar bildtexten
 	}; // Slut visa bilden
 
 	// Visa föregående bild
 	prevImage() {
-		if (this.imgIx > 0) this.imgIx--;
-		else this.imgIx = this.list.imgUrl.length - 1; // Gå runt till sista bilden
+		if (this.imgIx > 0) this.imgIx--; //Om bildindexet är större än noll backa till föregående
+		else this.imgIx = this.list.imgUrl.length - 1; // Annars gå runt till sista bilden
 		this.showImage();
 	}; // Slut visa föregående bild
 
 	// Visa nästa bild
 	nextImage() {
-		if (this.imgIx < this.list.imgUrl.length - 1) this.imgIx++;
-		else this.imgIx = 0; // Gå runt till första bilden
+		if (this.imgIx < this.list.imgUrl.length - 1) this.imgIx++; //Om aktuell bild inte är den sista stega frammåt
+		else this.imgIx = 0; // Annars gå runt till första bilden
 		this.showImage();
 	}; // Slut visa nästa bild
 
@@ -92,7 +93,8 @@ class ImageViewer {
 
 //-------Initiering av händelsehanterare
 function init() {
-	imgViewer = new ImageViewer("imgViewer");
+	imgViewer = new ImageViewer("imgViewer");//Initiera objektet
+	//Skapar händelsehanterar för menyn
 	document.querySelector("#categoryMenu").addEventListener("change",
 		function () {
 			imgViewer.requestImages("xml/images" + this.selectedIndex + ".xml");
@@ -100,8 +102,9 @@ function init() {
 
 		}
 	);
-	document.querySelector("#prevBtn").addEventListener("click", function () { imgViewer.prevImage(); });
-	document.querySelector("#nextBtn").addEventListener("click", function () { imgViewer.nextImage() });
+	//Skapar "piltangenter"
+	document.querySelector("#prevBtn").addEventListener("click", function () { imgViewer.prevImage(); }); //Bakåtpil
+	document.querySelector("#nextBtn").addEventListener("click", function () { imgViewer.nextImage() });//Frammåtpil
 	//---Slut på initiering av händelsehanterare
 
 
