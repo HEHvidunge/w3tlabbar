cb05429456d40a77398fc560a1569d5156945a46
HEHvidunge <hh222zd@student.lnu.se>
2023-10-23 12:52:38 +0200

    monday23okt

--- L4/js/script.js
+++ L4/js/script.js
@@ -2,7 +2,6 @@
 class ImageViewer {
 	constructor(id) {
 		this.titleElem = document.querySelector("#" + id + " h3"),//Skapar referens för rubrik
-
 			this.imgElem = document.querySelector("#" + id + " img"),//Skapar referns för bild
 			this.captionElem = document.querySelector("#" + id + " p"),//Skapar referns för bildtext
 			this.category = ""; //Inleder mot tom kategori
@@ -12,9 +11,10 @@ class ImageViewer {
 			imgCaption: [""]//Inledande värde
 		};
 		this.imgIx = 0,
-			this.timer = null
+			this.timer = null;
+		console.log(this);
+	}
 
-	};
 	//------Metoder kopplade till objektet ImageViewer------
 	// Ajax-anrop för att läsa in begärd fil
 	requestImages(file) {
@@ -35,47 +35,51 @@ class ImageViewer {
 		let captionElems = XMLcode.getElementsByTagName("caption"); // Alla caption-element
 
 		//Inläsning av data som är gemensam för alla instanser av objektet
-		imgViewer.category = XMLcode.getElementsByTagName("category")[0].firstChild.data;//Hämtar kategorinamnet
-		this.titleElem.innerHTML = imgViewer.category;//Referens för visning av kategorinamnet
-console.log(this.titleElem.innerHTML);
+		this.category = XMLcode.getElementsByTagName("category")[0].firstChild.data;//Hämtar kategorinamnet
+		this.titleElem.innerHTML = this.category;//Referens för visning av kategorinamnet
+		//console.log(this.titleElem.innerHTML);
 
 		//imgViewer.list.imgUrl = [];//Tömmer bildlistan
 		//imgViewer.list.imgCaption = [];//Tömmer bildtextlistan
 		for (let i = 0; i < urlElems.length; i++) {
-			let obj = new ImageViewer(urlElems[i].firstChild.data, captionElems[i].firstChild.data);
+			//let obj = new ImageViewer(urlElems[i].firstChild.data, captionElems[i].firstChild.data);
+			let obj = new ImageViewer("imgViewer");
+			obj.list.imgUrl.push(urlElems[i].firstChild.data);
+			obj.list.imgCaption.push(captionElems[i].firstChild.data);
+			//obj.push(urlElems[i].firstChild.data,captionElems[i].firstChild.data);
+			console.log(obj);
 		};
-		imgViewer.push(obj);
-		//imgViewer.list.imgUrl[i] = (urlElems[i].firstChild.data);
+
 		//imgViewer.list.imgCaption[i] = (captionElems[i].firstChild.data);
 		console.log(this);
-	}
-	this.imgIx = 0,//Nollställer bildindex
+		this.imgIx = 0,//Nollställer bildindex
+			this.showImage; // Anropar funktion för att visa första bilden
+	};
 
 
-this.showImage; // Anropar funktion för att visa första bilden
-	} // Slut
 
-// Visa bilden med index imgIx
-showImage() {
 
-	this.imgElem.src = this.list.imgUrl[this.imgIx];//Visar bilden
+	// Visa bilden med index imgIx
+	showImage() {
 
-	this.captionElem.innerHTML = (this.imgIx + 1) + ". " + this.list.imgCaption[this.imgIx]; //Visar bildtexten
-}; // Slut visa bilden
+		this.imgElem.src = this.list.imgUrl[this.imgIx];//Visar bilden
 
-// Visa föregående bild
-prevImage() {
-	if (this.imgIx > 0) this.imgIx--; //Om bildindexet är större än noll backa till föregående
-	else this.imgIx = this.list.imgUrl.length - 1; // Annars gå runt till sista bilden
-	this.showImage();
-}; // Slut visa föregående bild
+		this.captionElem.innerHTML = (this.imgIx + 1) + ". " + this.list.imgCaption[this.imgIx]; //Visar bildtexten
+	}; // Slut visa bilden
 
-// Visa nästa bild
-nextImage() {
-	if (this.imgIx < this.list.imgUrl.length - 1) this.imgIx++; //Om aktuell bild inte är den sista stega frammåt
-	else this.imgIx = 0; // Annars gå runt till första bilden
-	this.showImage();
-}; // Slut visa nästa bild
+	// Visa föregående bild
+	prevImage() {
+		if (this.imgIx > 0) this.imgIx--; //Om bildindexet är större än noll backa till föregående
+		else this.imgIx = this.list.imgUrl.length - 1; // Annars gå runt till sista bilden
+		this.showImage();
+	}; // Slut visa föregående bild
+
+	// Visa nästa bild
+	nextImage() {
+		if (this.imgIx < this.list.imgUrl.length - 1) this.imgIx++; //Om aktuell bild inte är den sista stega frammåt
+		else this.imgIx = 0; // Annars gå runt till första bilden
+		this.showImage();
+	}; // Slut visa nästa bild
 
 	// ----- Extramerit -----
 	/* Ta bort kommentaren kring koden, för att testa funktionaliteten för extrameriten
@@ -92,9 +96,9 @@ nextImage() {
 		}
 	} // End autoImage
 	*/
-}
-// ----- Slut Constructorfunktion -----
 
+	// ----- Slut Constructorfunktion -----
+}
 //-------Initiering och skapande av händelsehanterare
 function init() {
 	imgViewer = new ImageViewer("imgViewer");//Initierar objektet
